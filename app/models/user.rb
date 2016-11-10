e9cb0a9ba4f9074b4aada866c9f0143f494bdeda
Bang Nguyen <bangnv77@gmail.com>
2016-11-10 00:34:48 -0800

    create users_controller and display user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/       
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  def full_name
+    name.blank? ? email : name
+  end
 end
